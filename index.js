afcdce37b80d0758b6b18db5a478fcdf76216464
Murylo Firmino <57185715+murylomf@users.noreply.github.com>
2024-06-08 10:44:35 -0300

    add try catch

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const puppeteer = require('puppeteer');
 
+
+//Alterar o ADVERSARIO conforme o jogo, 
+//atenção aos que tem nome composto, por exemplo, o atletico mineiro, precisa ser atletico-mg
 const ADVERSARIO = 'fortaleza';
 const COMPETICAO = 'br24';
 const SETOR = 'sul';
 var tentativa = 0;
 
 (async () => {
-    // Create a browser instance
-    console.log("Create a browser instance")
-    const browser = await puppeteer.launch({ headless: false })
+    try {
+        // Create a browser instance
+        console.log("Create a browser instance")
+        const browser = await puppeteer.launch({ headless: false })
 
-    // Create a new page
-    const page = await browser.newPage();
+        // Create a new page
+        const page = await browser.newPage();
 
-    // Set viewport width and height
-    await page.setViewport({ width: 1280, height: 720 });
+        // Set viewport width and height
+        await page.setViewport({ width: 1280, height: 720 });
 
-    const website_url = `https://www.fieltorcedor.com.br/auth/login/`;
+        const website_url = `https://www.fieltorcedor.com.br/auth/login/`;
 
-    // Open URL in current page
-    await Login(page, website_url);
+        // Open URL in current page
+        await Login(page, website_url);
 
-    await FillForm(page, browser);
+        await FillForm(page, browser);
+    }
+
+    catch (e) {
+        console.error("Erro no processamento", e.message);
+    }
 
 })();
 
@@ -49,41 +58,55 @@ async function preencheCheck(page) {
 }
 
 async function FillForm(page, browser) {
-    await page.goto(`https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/categoria/`);
+    try {
+        await page.goto(`https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/categoria/`);
 
-    const comprarButton = await page.$('.btn.btn-link');
-    await comprarButton.click();
-    await page.waitForNavigation(); // vai ir para os setores
+        const comprarButton = await page.$('.btn.btn-link');
+        await comprarButton.click();
+        await page.waitForNavigation(); // vai ir para os setores
 
-    await page.goto(`https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/setor/${SETOR}/modo-de-compra/`); // forçar setor
+        await page.goto(`https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/setor/${SETOR}/modo-de-compra/`); // forçar setor
 
-    //await page.waitForNavigation();
+        //await page.waitForNavigation();
 
-    const prosseguirButton = await page.$('.btn.btn-primary');
-    await prosseguirButton.click();
-    await page.waitForNavigation();
+        const prosseguirButton = await page.$('.btn.btn-primary');
+        await prosseguirButton.click();
+        await page.waitForNavigation();
 
-    await preencheCheck(page);
-
-    let url = page.url();
-
-    while (url === `https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/setor/${SETOR}/`) {
         await preencheCheck(page);
-        url = page.url();
-    }
 
-    await browser.close();
+        let url = page.url();
+
+        while (url === `https://www.fieltorcedor.com.br/jogos/corinthians-x-${ADVERSARIO}-${COMPETICAO}/setor/${SETOR}/`) {
+            await preencheCheck(page);
+            url = page.url();
+        }
+
+        await browser.close();
+    }
+    catch (error) {
+        console.log('erro ao preencher: ', error.message);
+        FillForm(page, browser);
+    }
 }
 
 
 async function Login(page, website_url) {
-    await page.goto(website_url);
+    try {
+        await page.goto(website_url);
 
-    const userName = await page.$$('input[name*="username"]');
-    await userName[0].type('34468727870');
+        const userName = await page.$$('input[name*="username"]');
+        await userName[0].type('34468727870');
 
-    const password = await page.$$('input[name*="password"]');
-    await password[0].type('Cesar@010203');
+        const password = await page.$$('input[name*="password"]');
+        await password[0].type('Cesar@010203');
+        await page.waitForNavigation();
+    }
+    catch (e) {
+        console.log('Erro ao fazer login: ', e.message);
+    }
+}
 
-    await page.waitForNavigation();
+function returnError(msg) {
+    throw new Error(msg);
 }
